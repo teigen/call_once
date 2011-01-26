366aef7308b8ebf96e852fe04e1e029e2c770a81
Jon-Anders Teigen <mail@jteigen.com>
2011-01-26 13:30:43 +0100

    whoops... use lift 2.2

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,7 +4,7 @@ import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
 
-  val liftVersion = "2.2-RC4"
+  val liftVersion = "2.2"
   val lift = "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default"
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
 }
